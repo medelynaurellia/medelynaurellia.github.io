14cc0044e1de5318e1e7295656f0de733dd37e3d
medelynaurellia <63649615+medelynaurellia@users.noreply.github.com>
2020-04-14 19:42:01 +0700

    Add files via upload

---
+++ js/billie.js
@@ -0,0 +1,24 @@
+function home(){
+  document.getElementById('full').style.display='none';
+  document.getElementById('full2').style.display='block';
+  
+}
+
+
+function merchandise(){
+  document.getElementById('full').style.display='none';
+  document.getElementById('full2').style.display='block';
+  
+}
+
+function album(){
+  document.getElementById('full').style.display='none';
+  document.getElementById('full2').style.display='block';
+  
+}
+
+function biography(){
+  document.getElementById('full').style.display='none';
+  document.getElementById('full2').style.display='block';
+  
+}
