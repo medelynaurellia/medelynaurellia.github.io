374450dae42def07009ec16f59c31862f472f80c
medelynaurellia <63649615+medelynaurellia@users.noreply.github.com>
2020-04-16 01:30:32 +0700

    Add files via upload

--- js/billie.js
+++ js/billie.js
@@ -22,3 +22,279 @@ function biography(){
   document.getElementById('full2').style.display='block';
   
 }
+
+function cast(X) {
+      document.getElementById('select').style.display = "block";
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			
+		}
+		
+function opennewt(X) {
+			document.getElementById('newtdetail').style.display = "block";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function opentina(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "block";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openqueenie(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "block";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openjacob(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "block";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function opengellert(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "block";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function opendumbledore(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "block";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		
+		function opentheseus(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "block";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function opencredence(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "block";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openmary(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "block";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openmeledictus(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "block";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openleta(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "block";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openyusuf(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "block";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openflamel(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "blcok";
+			document.getElementById('seraphinadetail').style.display = "none";
+			document.getElementById('select').style.display = "none";
+		}
+		
+		function openseraphina(X) {
+			document.getElementById('newtdetail').style.display = "none";
+			document.getElementById('tinadetail').style.display = "none";
+			document.getElementById('queeniedetail').style.display = "none";
+			document.getElementById('jacobdetail').style.display = "none";
+			document.getElementById('grindelwalddetail').style.display = "none";
+			document.getElementById('dumbledoredetail').style.display = "none";
+			document.getElementById('theseusdetail').style.display = "none";
+			document.getElementById('credencedetail').style.display = "none";
+			document.getElementById('marydetail').style.display = "none";
+			document.getElementById('meledictusdetail').style.display = "none";
+			document.getElementById('letadetail').style.display = "none";
+			document.getElementById('yusufdetail').style.display = "none";
+			document.getElementById('flameldetail').style.display = "none";
+			document.getElementById('seraphinadetail').style.display = "block";
+			document.getElementById('select').style.display = "none";
+		}
+
+
+	function scroll(X) {
+	  setTimeout(function(), 3000)}
